7fe5bd8dd36dfb6a6e2cc2dee3911ffb77cde0cc
wm <278188438@qq.com>
2016-08-18 10:30:54 +0800

    vue-cart

---
+++ vue-test3/js/data.js
@@ -0,0 +1,21 @@
+var data = [{
+    id: 1,
+    name: "iphone5",
+    jiage: 4999,
+    shuliang: 1
+}, {
+    id: 2,
+    name: "iphone6",
+    jiage: 5999,
+    shuliang: 1
+}, {
+    id: 1,
+    name: "iphone7",
+    jiage: 9999,
+    shuliang: 1
+}, {
+    id: 1,
+    name: "ipad",
+    jiage: 1899,
+    shuliang: 1
+}];
